24917c6ad1144222b20a2afc9c15b6f194b21b20
David Fernández <davidfernandez@MacBook-Pro.local>
2024-02-06 20:29:13 +0100

    Solved reverse hexadecimal

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -31,33 +31,40 @@ static int  ft_len_nbr(int n)
     return (size);
 }
 
-int	ft_puthex_ptr(void *n, int fd)
+int	ft_puthex_ptr(unsigned long long n, int fd)
 {
 	unsigned long long	address;
-	int	rest;
 	int	len;
 	
 	address = (unsigned long long)n;
 	len = ft_len_nbr(address) + 2;
-	//printf("La longitud de la address es: %d\n", len);
-	write(fd, "0x", 2);
-	while (len > 0)
+
+	if (address >= 16)
+	{
+		ft_puthex_ptr(address / 16, fd);
+		ft_puthex_ptr(address % 16, fd);
+	}
+	else
+	{
+		if (address < 10)
+			ft_putchar_fd(address + '0', fd);
+		else
+			ft_putchar_fd(address - 10 + 'a', fd);		
+	}
+	
+
+	/* while (len-- > 1)
 	{
 		rest = address % 16;
 		char hex_digit;
 		if (rest < 10)
-		{
 			hex_digit = rest + '0';
-		}
 		else
-		{
 			hex_digit = rest - 10 + 'a';
-		}
-		ft_putchar_fd(hex_digit, fd);
+		write(fd, &hex_digit, 1);
 		address /= 16;
-		len--;
-	}
-	return (0);
+	} */
+	return (len);
 }
 
 int	ft_printf(char const *str, ...)
@@ -79,7 +86,10 @@ int	ft_printf(char const *str, ...)
 				//ft_putstr_fd(va_arg(args, char *), 1);
 			}
 			else if (str[i] == 'p')
-				ft_puthex_ptr(va_arg(args, void *), 1);
+			{
+				write(1, "0x", 2);
+				ft_puthex_ptr(va_arg(args, unsigned long long), 1);
+			}
 			else if (str[i] == 'd' || str[i] == 'i')
 				ft_putnbr_fd(va_arg(args, int), 1);
 			else if (str[i] == 'u')
@@ -96,7 +106,7 @@ int	ft_printf(char const *str, ...)
 		i++;
 	}
 	va_end(args);
-	return (0);
+	return (i);
 }
 
 int main(void)
@@ -109,6 +119,6 @@ int main(void)
 	//ft_printf("Own function result: %c %% %d\n%d\n%X\n", c, x, min, &hex);
 	//printf("Original result: %c %% %d\n%d\n%X\n", c, x, min, &hex);
 	
-	ft_printf("%p", &min);
-	printf("\n%p", &min);
+	ft_printf("Own: %p", &x);
+	printf("\nOriginal: %p\n", &x);
 }
