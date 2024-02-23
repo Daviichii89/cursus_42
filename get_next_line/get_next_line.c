8cc2fba6cd0a78b17ea8aa93179c96b4ab6f7d8b
Daviichii89 <davichi89@gmail.com>
2024-02-23 20:34:06 +0100

    Fix issues of norminette

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,35 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davifer2 <davifer2@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 10:15:57 by davifer2          #+#    #+#             */
+/*   Updated: 2024/02/23 20:22:57 by davifer2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	char *		read_content;
-	int		read_bytes;
-	static char *	read_buffer;
+	char		*read_content;
+	int			read_bytes;
+	static char	*read_buffer;
 
 	read_bytes = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return(NULL);
+		return (NULL);
 	read_content = (char *)malloc(BUFFER_SIZE + 1 * sizeof(char));
 	if (!read_content)
-		return(NULL);
+		return (NULL);
 	while (!(ft_strchr(read_buffer, '\n')) && read_bytes != 0)
 	{
 		read_bytes = read(fd, read_content, BUFFER_SIZE);
-		if (read_bytes = -1)
+		if (read_bytes == -1)
 		{
-			read_buffer = free-all(read_content, read_buffer);
+			read_buffer = free_all(read_content, read_buffer);
 			return (NULL);
 		}
 		*(read_content + read_bytes) = '\0';
 		read_buffer = ft_strjoin(read_buffer, read_content);
 	}
-	free(read_content);
+	free (read_content);
 	read_content = read_line(read_buffer);
 	read_buffer = create_substring(read_buffer);
-	return (read_content);	
+	return (read_content);
 }
-
+/*
 int	main(void)
 {
-	get_next_line();
-}
+	int		fd;
+	char	*line;
+	int		i;
+
+	fd = open("text.txt", 0_RDWR);
+	if (fd < 0)
+		return (1);
+	i = 1;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL || i > 80)
+			break;
+		printf("line %d: "%s", i++, line);
+		free(line);
+	}
+	printf("line %d: %s", i++, line);
+	close(fd);
+	return (0);
+}*/
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,47 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davifer2 <davifer2@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 10:15:57 by davifer2          #+#    #+#             */
+/*   Updated: 2024/02/23 20:22:57 by davifer2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "get_next_line.h"
 
-size_t ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    while (*s != '\0')
-    {
-        if (*s == (char)c)
-            return ((char *)s);
-        s++;
-    }
-    if (*s == (char)c)
-        return ((char *)s);
-    else
-        return (0);
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	else
+		return (0);
 }
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char *str;
-    int i;
-    int j;
-    int len;
+	char	*str;
+	int		i;
+	int		j;
+	int		len;
 
-    if (!s1 || !s2)
-        return (0);
-    i = ft_strlen(s1);
-    j = ft_strlen(s2);
-    len = i + j + 1;
-    str = (char *)malloc(len * sizeof(char));
-    if (!str)
-        return (0);
-    ft_strlcpy(str, (char *)s1, i + 1);
-    ft_strlcat(&str[i], (char *)s2, j + 1);
-    return (str);
+	if (!s1 || !s2)
+		return (0);
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	len = i + j + 1;
+	str = (char *)malloc(len * sizeof(char));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, (char *)s1, i + 1);
+	ft_strlcat(&str[i], (char *)s2, j + 1);
+	return (str);
 }
