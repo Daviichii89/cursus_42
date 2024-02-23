03a3301b7a30c6a6eebad03af3970b66827f17fb
Daviichii89 <davichi89@gmail.com>
2024-02-23 20:38:15 +0100

    Add headers on utils

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -13,6 +13,10 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE 
+#  define BUFFER_SIZE 8
+# endif
+
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -20,5 +24,9 @@
 # include <limits.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
 
 #endif
