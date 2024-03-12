c0c9e1da0d2c9aec09038038baf57a7f8bee216e
David Fernández Mercader <davichi89@gmail.com>
2024-03-12 16:14:29 +0100

    Add main to create a window

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,15 +6,18 @@
 /*   By: davifer2 <davifer2@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 14:31:55 by davifer2          #+#    #+#             */
-/*   Updated: 2024/03/12 14:33:22 by davifer2         ###   ########.fr       */
+/*   Updated: 2024/03/12 16:09:39 by davifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "mlx/mlx.h"
 
 int main(void)
 {
 	void	*mlx;
+	void	*mlx_window;
 
 	mlx = mlx_init();
+	mlx_window = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
