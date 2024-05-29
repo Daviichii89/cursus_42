647ea701f4a5abaf6ef2a6a271ac141499617651
David Fernández Mercader <davichi89@gmail.com>
2024-05-29 20:42:47 +0200

    Update Makefile

--- so_long/check_map.d
+++
@@ -1,2 +0,0 @@
-check_map.o: check_map.c so_long.h mlx/mlx.h libft/inc/libft.h \
-  libft/inc/ft_printf.h libft/inc/get_next_line.h
--- so_long/so_long_utils.d
+++
@@ -1,2 +0,0 @@
-so_long_utils.o: so_long_utils.c so_long.h mlx/mlx.h libft/inc/libft.h \
-  libft/inc/ft_printf.h libft/inc/get_next_line.h
