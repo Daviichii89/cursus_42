51d8772f822653f6928e00ceb3692cd910cd84f2
David Fernández Mercader <davichi89@gmail.com>
2024-05-31 12:47:52 +0200

    Solved issue with passing folder map

--- so_long/deps/moves.d
+++
@@ -1,2 +0,0 @@
-objects/moves.o: moves.c so_long.h mlx/mlx.h libft/inc/libft.h \
-  libft/inc/ft_printf.h libft/inc/get_next_line.h
--- so_long/deps/render_map.d
+++
@@ -1,2 +0,0 @@
-objects/render_map.o: render_map.c so_long.h mlx/mlx.h libft/inc/libft.h \
-  libft/inc/ft_printf.h libft/inc/get_next_line.h
