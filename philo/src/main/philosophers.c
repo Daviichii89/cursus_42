1b8f0249736abb5099842bc777da087f18459830
Daviichii89 <davichi89@gmail.com>
2024-10-22 20:34:07 +0200

    Created free functions

--- philo/src/main/philosophers.c
+++ philo/src/main/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: davifer2 <davifer2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 12:41:03 by davifer2          #+#    #+#             */
-/*   Updated: 2024/09/04 22:44:44 by davifer2         ###   ########.fr       */
+/*   Updated: 2024/10/22 20:03:39 by davifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ int	main(int argc, char **argv)
 
 	if (argc != 5 && argc != 6)
 		return (error_msg("Wrong input!\nUsage: ./philo 4 800 400 400 [5]\n"));
-	check_arg(&data, argv);
-	if (init_data(&data))
+	
+	if (check_arg(&data, argv) || init_data(&data))
 		return (1);
 	start_simulation(&data);
-	// free_all(&data);
+	free_philos(&data, data.n_philos);
+	free_forks(data.forks, data.n_philos);
+	pthread_mutex_destroy(&data.print_mtx);
 	return (0);
 }
--- philo/src/utils/free_all.c
+++ philo/src/utils/free_all.c
@@ -6,14 +6,35 @@
 /*   By: davifer2 <davifer2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 23:41:03 by davifer2          #+#    #+#             */
-/*   Updated: 2024/09/04 19:47:38 by davifer2         ###   ########.fr       */
+/*   Updated: 2024/10/22 20:05:53 by davifer2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	free_all(t_data *data)
+void    free_forks(t_mutex **forks, int n_forks)
 {
-	free(data->philos);
-    free(data);
+    int i;
+
+    i = -1;
+    while (++i != n_forks)
+        pthread_mutex_destroy(&forks[i]);
+    free(*forks);
+    *forks = NULL;
+}
+
+int free_philos(t_data *data, long n_philos)
+{
+    long    i;
+
+    i = -1;
+    while (++i < n_philos)
+    {
+        if (pthread_mutex_destroy(&data->philos[n_philos].death_check) == -1)
+            break ;
+        if (pthread_mutex_destroy(&data->philos[n_philos].status_mtx) == -1)
+            break ;
+    }
+    free(data->philos);
+    return (-1);
 }
